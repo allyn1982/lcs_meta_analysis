362333ad5776bc294389f023f1ae9345f33f4d99
Yannan Lin <37195634+allyn1982@users.noreply.github.com>
2022-06-02 16:10:09 -0700

    Update meta_other_variables.R

--- meta_other_variables.R
+++ meta_other_variables.R
@@ -1,13 +1,13 @@
 library(meta)
 library(metafor)
-source('C:/Users/yanna/Desktop/Submission/code/functions.R')
+source('~/functions.R')
 
 ####################
 #### preprocess ####
 ####################
 
 # load data
-data <- read.csv('C:/Users/yanna/Desktop/Submission/data/meta_other_variables.csv')
+data <- read.csv('~/meta_other_variables.csv')
 
 sex_defined_14 <- subset(data, data$type=='defined'&data$variable=='sex' & data$lungrads=='Lung-RADS 1-4')
 sex_anytime_12 <- subset(data, data$type=='anytime'&data$variable=='sex' & data$lungrads=='Lung-RADS 1-2')
@@ -26,9 +26,9 @@ smoke_defined_14 <- subset(data, data$type=='defined'&data$variable=='smoking' &
 # sex - defined 14
 m_sex_defined_14 <- metaprop_model_sub(sex_defined_14$n_adherent,
                                sex_defined_14$n_total,
-                               sex_defined_14$ï..study_lungrads,
+                               sex_defined_14$Ã¯..study_lungrads,
                                sex_defined_14$category)
-forest_plot_sub(m_sex_defined_14, sex_defined_14$ï..study_lungrads, "Defined \nAdherence rate")
+forest_plot_sub(m_sex_defined_14, sex_defined_14$Ã¯..study_lungrads, "Defined \nAdherence rate")
 sum(sex_defined_14$n_total) #2079
 sum(sex_defined_14$n_total[sex_defined_14$category=='Sex: Male']) #1255
 sum(sex_defined_14$n_total[sex_defined_14$category=='Sex: Female']) #824
@@ -37,9 +37,9 @@ sum(sex_defined_14$n_total[sex_defined_14$category=='Sex: Female']) #824
 # race - defined 14
 m_race_defined_14 <- metaprop_model_sub(race_defined_14$n_adherent,
                                         race_defined_14$n_total,
-                                        race_defined_14$ï..study_lungrads,
+                                        race_defined_14$Ã¯..study_lungrads,
                                         race_defined_14$category)
-forest_plot_sub(m_race_defined_14, race_defined_14$ï..study_lungrads, "Defined \nAdherence rate")
+forest_plot_sub(m_race_defined_14, race_defined_14$Ã¯..study_lungrads, "Defined \nAdherence rate")
 sum(race_defined_14$n_total) #1607
 sum(race_defined_14$n_total[race_defined_14$category=='Race: White']) #1133
 sum(race_defined_14$n_total[race_defined_14$category=='Race: Non-White']) #474
@@ -47,9 +47,9 @@ sum(race_defined_14$n_total[race_defined_14$category=='Race: Non-White']) #474
 # race - defined_12
 m_race_defined_12 <- metaprop_model_sub(race_defined_12$n_adherent,
                                         race_defined_12$n_total,
-                                        race_defined_12$ï..study_lungrads,
+                                        race_defined_12$Ã¯..study_lungrads,
                                         race_defined_12$category)
-forest_plot_sub(m_race_defined_12, race_defined_12$ï..study_lungrads, "Defined \nAdherence rate")
+forest_plot_sub(m_race_defined_12, race_defined_12$Ã¯..study_lungrads, "Defined \nAdherence rate")
 sum(race_defined_12$n_total) #1218
 sum(race_defined_12$n_total[race_defined_12$category=='Race: White']) #973
 sum(race_defined_12$n_total[race_defined_12$category=='Race: Non-White']) #245
@@ -58,9 +58,9 @@ sum(race_defined_12$n_total[race_defined_12$category=='Race: Non-White']) #245
 # smoke_defined_14
 m_smoke_defined_14 <- metaprop_model_sub(smoke_defined_14$n_adherent,
                                          smoke_defined_14$n_total,
-                                         smoke_defined_14$ï..study_lungrads,
+                                         smoke_defined_14$Ã¯..study_lungrads,
                                          smoke_defined_14$category)
-forest_plot_sub(m_smoke_defined_14, smoke_defined_14$ï..study_lungrads, "Defined \nAdherence rate")
+forest_plot_sub(m_smoke_defined_14, smoke_defined_14$Ã¯..study_lungrads, "Defined \nAdherence rate")
 sum(smoke_defined_14$n_total) #2079
 sum(smoke_defined_14$n_total[smoke_defined_14$category=='Smoking status: Current smokers']) #1051
 sum(smoke_defined_14$n_total[smoke_defined_14$category=='Smoking status: Former smokers']) #1028
